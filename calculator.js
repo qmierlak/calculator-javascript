961ea89e5c3434f54b171f228319de9c958f8180
Quinn <qmierlak@gmail.com>
2024-08-13 10:28:53 -0500

    Add buttons to DOM

--- calculator.js
+++ calculator.js
@@ -1,1 +1,37 @@
 console.log("Hello world")
+
+
+function add(a, b) {
+    const result = a + b;
+    console.log(result);
+}
+function subtract(a, b) {
+    const result = a - b;
+    console.log(result);
+}
+function multiply(a, b) {
+    const result = a * b;
+    console.log(result);
+}
+function divide(a, b) {
+    const result = a / b;
+    console.log(result);
+}
+
+
+function operate(a, b) {
+    // calls an above function
+    add(a,b);
+}
+
+const num1 = 0;
+const operator = null;
+const num2 = 0;
+
+let displayVal = 0;
+
+
+// add(5,3)
+// subtract(5,3)
+// multiply(5,3)
+// divide(20,5)
